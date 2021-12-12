14f4b6f17d39dd048d7504113b84a4673fe76ebf
Jisu Han <weblite@naver.com>
2021-12-12 23:39:37 +0900

    Peterson's solution 복습 -> 가독성 좋게 바꿈

--- src/synchronization/PetersonSolution.java
+++ src/synchronization/PetersonSolution.java
@@ -12,20 +12,20 @@ public class PetersonSolution {
 
     public void process1(){
         flag[1]=true;
-        int p2=2;  //프로세스2를 2로 표현
+        int process2=2;  //프로세스2를 2로 표현
         turn=2;   // 다음 2번 프로세스 실행
-        while(flag[2]==true && turn==p2){
+        while(flag[2]==true && turn==process2){
             // 프로세스 p2의 차례이고, flag가 준비가 되었어도, p1이 먼저 실행되어야 하기때문에 여기서 대기
         }
         System.out.println("여기는 critical section, 프로세스가 필요한 데이터를 처리하도록.");
         flag[1]=false; // 다음 프로세스가 사용하도록
     }
-
+// flag와 turn을 다르게 설정함으로써 , 각 프로세스의 코드들이 실행되면서 어떤 코드가 우선순위이던, 하나만 critical section에 진입하게 된다.
     public void process2(){
         flag[2]=true;
-        int p1=1; // 프로세스 1을 1로 표햔
+        int process1=1; // 프로세스 1을 1로 표햔
         turn=1;
-        while(flag[1]==true && turn==p1) {
+        while(flag[1]==true && turn==process1) {
             // 프로세스 p1의 차례이고, flag가 준비가 되었어도, p2가 먼저 실행되어야 하기때문에 여기서 대기
         }
         System.out.println("여기는 critical section, 프로세스가 필요한 데이터를 처리하도록.");
